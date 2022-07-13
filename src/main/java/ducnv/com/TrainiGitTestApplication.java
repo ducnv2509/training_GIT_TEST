7a0bcb2f50d4995d8d4820446b1cd1210a517317
ducnv2509 <ducit2509@gmail.com>
2022-07-13 22:21:14 +0700

    udpate

--- src/main/java/ducnv/com/TrainiGitTestApplication.java
+++ src/main/java/ducnv/com/TrainiGitTestApplication.java
@@ -8,5 +8,4 @@ public class TrainiGitTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TrainiGitTestApplication.class, args);
     }
-
 }
--- src/test/java/ducnv/com/TestDemo.java
+++ src/test/java/ducnv/com/TestDemo.java
@@ -3,5 +3,6 @@ package ducnv.com;
 public class TestDemo {
     public static void main(String[] args) {
         System.out.println("huong");
+        System.out.println("Hello _ Huong ");
     }
 }
